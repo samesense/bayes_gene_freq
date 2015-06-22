b97bb5705c535e77ceb35918e2d705aaef1b000a
Perry <samesense@gmail.com>
2015-06-22 17:21:42 -0400

    ?

--- code/sf.py
+++ code/sf.py
@@ -1,17 +1,29 @@
 """Snakefile."""
 
+WORK = '../working/'
+PLOTS = '../plots/'
+
+rule clean:
+    run:
+        for afile in (WORK + 'target.trace', \
+                      PLOTS + 'target.png', \
+                      WORK + 'esp.trace', \
+                      PLOTS + 'esp.png', \
+                      PLOTS + 'cmp.png'):
+            shell('rm ' + afile)
+
 rule traceTarget:
-    output: '../working/target.trace',
-            '../plots/target.png'
+    output: WORK + 'target.trace', \
+            PLOTS + 'target.png'
     shell: 'python traceTarget.py {output}'
 
 rule traceEsp:
-    output: '../working/esp.trace',
-            '../plots/esp.png'
+    output: WORK + 'esp.trace', \
+            PLOTS + 'esp.png'
     shell: 'python traceEsp.py {output}'
 
 rule cmpTraces:    
-    input: '../working/target.trace', \
-           '../working/esp.trace'
-    output: '../plots/cmp.png'
+    input: WORK + 'target.trace', \
+           WORK + 'esp.trace'
+    output: PLOTS + 'cmp.png'
     shell: 'python cmpTraces.py {input} {output}'
